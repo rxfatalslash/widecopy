45c8cdff77223aad89e120e96227033316e9cefa
rxfatalslash <aaronsanchezmenendez@gmail.com>
2023-09-15 14:36:00 +0200

    Cambio de rango de IPs

--- copyfile.sh
+++ copyfile.sh
@@ -4,20 +4,15 @@
 filename=$1
 dest_folder=$2
 
-# Get a list of all devices in the network
-devices=$(ip addr | grep -E "inet (192.168.0.[0-9]+)" | tr -d " " | sort)
+# Devices
+devices="192.168.1.[•10-150]"
 
-# Loop through the devices and copy the file
-success_count=0
-fail_count=0
+# Open multiple SSH sessions
 for device in $devices; do
-    echo "Copying file to $device"
+    echo "Opening SSH session to $device"
     ssh $device "mkdir -p $dest_folder"
-    if scp $filename $device:$dest_folder; then
-        echo "Success: $device (IP: $ip)"
-        success_count=$((success_count + 1))
-    else
-        echo "Fail: $device (IP: $ip)"
-        fail_count=$((fail_count + 1))
-    fi
+    ssh $device "scp $filename $dest_folder" &
 done
+
+# Wait for all SSH sessions to finish
+wait
