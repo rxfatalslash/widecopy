37d170a01977abe848dd1e90f04d909b3d28915b
rxfatalslash <aaronsanchezmenendez@gmail.com>
2023-10-12 14:17:43 +0200

    Logo

--- copyfile.sh
+++ copyfile.sh
@@ -2,17 +2,37 @@
 
 CRE=$(tput setaf 1)
 CGR=$(tput setaf 2)
+BLD=$(tput bold)
 CNC=$(tput sgr0)
 
 # Get the arguments
 filename=$1
 dest_folder=$2
 
+logo () {
+
+    local text="${1:?}"
+    echo "
+    WOdddddxk0KNW                                            WNK0kxdddddOW 
+     Wo........',:lx0N                                    N0xl:,'........oW 
+      Xc.,clcc:;'....;lON                              NOl;....';:cclc,.cX  
+       Kc'cdodlc::ccc;..:kN                          Nk:..;ccc::cldodc'cK   
+        Xo;:loooooooool:,'cK                        Kc',:loooooooool:;oX    
+         WKxolccclllllllc;';OW                    WO;';clllllllcccloxKW     
+            WNKOOOOkkkOkkkdokN                    NkodkkkOkkkOOOOKNW       
+    \n\n"
+    printf ' %s [%s%s %s%s %s]%s\n\n' "${CRE}" "${CNC}" "${CYE}" "${text}" "${CNC}" "${CRE}" "${CNC}"
+}
+
 # List of devices
 devices=()
 for octeto in {10..150}; do
     devices+=("192.168.1.$octeto")
 done
+
+logo "rxfatalslash"
+printf "%s%sIniciando proceso de copia...%s" "${BLD}" "${CRE}" "${CNC}"
+sleep 3
 
 # Open multiple SSH sessions
 for device in "${devices[@]}"; do
@@ -24,8 +44,3 @@ for device in "${devices[@]}"; do
         printf "$device [%sFAIL%s]\n" "${CRE}" ${CNC}
     fi
 done
-
-# Wait for all SSH sessions to finish
-wait
-
-echo "Tarea terminada con éxito"
